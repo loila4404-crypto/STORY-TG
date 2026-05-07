94754c3f393f32c3c205f6de6b568f191f668432
loila4404-crypto <loila4404@gmail.com>
2026-05-07 17:28:16 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -30,16 +30,19 @@ def get_accounts_dict():
 
 
 def save_account(account_name, data):
-    supabase.table("accounts").upsert({
-        "owner_id": data.get("owner_id"),
-        "account_name": account_name,
-        "display_name": data.get("display_name"),
-        "phone": data.get("phone"),
-        "telegram_id": data.get("telegram_id"),
-        "username": data.get("username"),
-        "first_name": data.get("first_name"),
-        "session_path": data.get("session"),
-    }).execute()
+    supabase.table("accounts").upsert(
+        {
+            "owner_id": data.get("owner_id"),
+            "account_name": account_name,
+            "display_name": data.get("display_name"),
+            "phone": data.get("phone"),
+            "telegram_id": data.get("telegram_id"),
+            "username": data.get("username"),
+            "first_name": data.get("first_name"),
+            "session_path": data.get("session"),
+        },
+        on_conflict="account_name"
+    ).execute()
 
 
 def delete_account(account_name):
